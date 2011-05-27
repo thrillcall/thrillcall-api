33b87614909bfd2d57edab1a82fbac415c2a2be1
Dan Healy <dan@beyondludus.com>
2011-05-27 16:22:07 -0700

    First working commit, it tests against localhost

--- lib/thrillcall-api.rb
+++ lib/thrillcall-api.rb
@@ -3,9 +3,60 @@
 # require 'nucleus_queue_format/response'
 # require 'nucleus_queue_format/exception'
 # require 'validatable'
+require 'faraday'
+require 'json'
 
-class ThrillcallAPI
-  def initialize
+module ThrillcallAPI
+  class << self
+    def new(api_key = "1234567890", base_url = "http://api.thrillcall.com/api/", version = 2)
+      @api_key  = api_key
+      
+      unless base_url.match /^http:\/\//
+        base_url = "http://" + base_url
+      end
+      
+      unless base_url.match /\/$/
+        base_url = base_url + "/"
+      end
+      
+      @headers = { 
+        :accept     => 'application/json',
+        :user_agent => "Thrillcall API Wrapper version #{ThrillcallAPI::VERSION}"
+      }
+      
+      @base   = "#{base_url}v#{version}/"
+      @conn   = Faraday.new( :url => @base, :headers => @headers ) do |builder|
+        builder.adapter Faraday.default_adapter
+        builder.response :logger
+        builder.response :raise_error
+      end
+      
+      return self
+    end
     
+    def conn
+      return @conn if @conn
+      self.new
+      return @conn
+    end
+    
+    def get(endpoint, params)
+      begin
+        r = conn.get do |req|
+          req.url endpoint, params.merge(:api_key => @api_key)
+        end
+        JSON.parse(r.body)
+      rescue Exception => e
+        puts "#{e}: #{e.backtrace.join("\n")}"
+      end
+    end
+    
+    def events(limit = 10, offset = 0)
+      get 'events', :limit => limit.to_s, :offset => offset.to_s
+    end
+    
+    def zip_codes(limit = 10, offset = 0)
+      get 'zip_codes', :limit => limit.to_s, :offset => offset.to_s
+    end
   end
 end
--- spec/thrillcall-api/thrillcall-api_spec.rb
+++ spec/thrillcall-api/thrillcall-api_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 require 'thrillcall-api'
 
+TEST_KEY  = "374a89fd4629d57c"
+LOCALHOST = "http://localhost:9292/api/"
+
 describe "ThrillcallAPI" do
   
-  it "should initialize properly with faraday"
+  it "should initialize properly with faraday" do
+    tc = nil
+    lambda { tc = ThrillcallAPI.new(TEST_KEY, LOCALHOST) }.should_not raise_error
+    puts tc.inspect
+    tc.conn.class.should == Faraday::Connection
+  end
+  
+  it "should get a list of events" do
+    tc = ThrillcallAPI.new(TEST_KEY, LOCALHOST)
+    e = tc.events(14)
+    e.length.should == 14
+  end
   
 end
